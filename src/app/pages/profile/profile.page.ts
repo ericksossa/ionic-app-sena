32a455e8f084b96b51fc82a76aab00b8ca7516b6
jonathan07 <jonathanflorez07@gmail.com>
2019-02-26 12:29:33 -0500

    se modificaron las paginas Home,Perfil,Chat

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -5,4 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: 'profile.page.html',
   styleUrls: ['profile.page.scss']
 })
-export class ProfilePage {}
+export class ProfilePage {
+
+  takePhoto() {
+  //   const options: CameraOptions = {
+  //     quality: 100,
+  //     destinationType: this.camera.DestinationType.DATA_URL,
+  //     encodingType: this.camera.EncodingType.JPEG,
+  //     mediaType: this.camera.MediaType.PICTURE,
+  //     correctOrientation: true
+  //   }
+
+  //   this.camera.getPicture(options).then((imageData) => {
+  //     // imageData is either a base64 encoded string or a file URI
+  //     // If it's base64 (DATA_URL):
+  //     let base64Image = 'data:image/jpeg;base64,' + imageData;
+  //     if (base64Image) {
+  //       this.modalCtrl.create(NewPostPage, {img: base64Image}).present();
+  //     }
+      
+  //   }, (err) => {
+  //     // Handle error
+  //   });
+
+  }
+
+  editPerfil(){
+    // this.modalCtrl.create(EditProfilePage).present();
+  }
+}
