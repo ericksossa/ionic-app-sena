f784622fd3894be3e641c6191b4d21ba20a2e2ce
erick <ericksossa09@gmail.com>
2019-02-28 07:18:44 -0500

    se instalo el plugin de la camara

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-profile',
@@ -6,31 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['profile.page.scss']
 })
 export class ProfilePage {
-
-  takePhoto() {
-  //   const options: CameraOptions = {
-  //     quality: 100,
-  //     destinationType: this.camera.DestinationType.DATA_URL,
-  //     encodingType: this.camera.EncodingType.JPEG,
-  //     mediaType: this.camera.MediaType.PICTURE,
-  //     correctOrientation: true
-  //   }
-
-  //   this.camera.getPicture(options).then((imageData) => {
-  //     // imageData is either a base64 encoded string or a file URI
-  //     // If it's base64 (DATA_URL):
-  //     let base64Image = 'data:image/jpeg;base64,' + imageData;
-  //     if (base64Image) {
-  //       this.modalCtrl.create(NewPostPage, {img: base64Image}).present();
-  //     }
-      
-  //   }, (err) => {
-  //     // Handle error
-  //   });
+  constructor(private camera: Camera) {
 
   }
 
-  editPerfil(){
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      const base64Image = 'data:image/jpeg;base64,' + imageData;
+      // if (base64Image) {
+      //   this.modalCtrl.create(NewPostPage, { img: base64Image }).present();
+      // }
+
+    }, (err) => {
+      // Handle error
+    });
+
+  }
+
+  editPerfil() {
     // this.modalCtrl.create(EditProfilePage).present();
   }
 }
