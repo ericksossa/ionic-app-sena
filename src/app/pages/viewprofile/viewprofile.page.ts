84b97064f166909c14051105f1b521cc9648f376
jonathan07 <jonathanflorez07@gmail.com>
2019-06-20 14:28:33 -0500

    Se agrego pagina viewprofile

---
+++ src/app/pages/viewprofile/viewprofile.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewprofile',
+  templateUrl: './viewprofile.page.html',
+  styleUrls: ['./viewprofile.page.scss'],
+})
+export class ViewprofilePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
